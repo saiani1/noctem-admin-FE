ce1c7c7287cce0f077a4298edf8ee433a1acfecf
Im-hass <huiseon4107@gmail.com>
2022-10-31 01:49:25 +0900

    Fix: SSE 시작 위치 변경 _app.tsx -> index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import type { AppProps } from 'next/app';
-import React, { useEffect } from 'react';
-import { toast, Toaster } from 'react-hot-toast';
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
 import { RecoilRoot } from 'recoil';
 
 import '../styles/index.scss';
@@ -9,30 +9,6 @@ import MenubarList from '../src/components/common/menubarList';
 import Header from '../src/components/common/header';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    if ('Notification' in window) {
-      Notification.requestPermission(); // 알림 권한 요청
-    }
-
-    const STREAM_URL = `https://sse.noctem.click:33333/sse/alert-server/store/1`;
-    const ssEvents = new EventSource(STREAM_URL);
-
-    ssEvents.addEventListener('message', event => {
-      const data = JSON.parse(event.data);
-      toast(data.message, {
-        icon: '📢',
-      });
-    });
-
-    ssEvents.addEventListener('error', err => {
-      console.log('ERR.', err);
-    });
-
-    return () => {
-      ssEvents.close();
-    };
-  }, []);
-
   return (
     <RecoilRoot>
       <noscript>You should use javascript</noscript>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
 /* eslint-disable react/function-component-definition */
-import React from 'react';
+import React, { useEffect } from 'react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { toast } from 'react-hot-toast';
 import Login from './login';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    if ('Notification' in window) {
+      Notification.requestPermission(); // 알림 권한 요청
+    }
+
+    const STREAM_URL = `https://sse.noctem.click:33333/sse/alert-server/store/1`;
+    const ssEvents = new EventSource(STREAM_URL);
+
+    ssEvents.addEventListener('message', event => {
+      const data = JSON.parse(event.data);
+      toast(data.message, {
+        icon: '📢',
+      });
+    });
+
+    ssEvents.addEventListener('error', err => {
+      console.log('ERR.', err);
+    });
+
+    return () => {
+      ssEvents.close();
+    };
+  }, []);
+
   return (
     <>
       <Head>
